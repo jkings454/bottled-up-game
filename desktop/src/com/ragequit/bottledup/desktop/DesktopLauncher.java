fc7cea3b1fbfa5467b0e9aba367c5610033765f1
Josh Nichols <josh.nichols.lives@gmail.com>
2018-05-05 10:01:03 -0600

    Inital Setup

---
+++ desktop/src/com/ragequit/bottledup/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ragequit.bottledup.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ragequit.bottledup.BottledUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BottledUp(), config);
+	}
+}
