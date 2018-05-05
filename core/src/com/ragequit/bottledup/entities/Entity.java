c2d1c206f52e9f4cb8a8e68486d233e77b7745f9
DavidKJones <emaildkj@gmail.com>
2018-05-05 13:10:51 -0600

    Implemented Entity base class.

--- core/src/com/ragequit/bottledup/entities/Entity.java
+++ core/src/com/ragequit/bottledup/entities/Entity.java
@@ -1,23 +1,34 @@
 package com.ragequit.bottledup.entities;
 
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.BodyDef.*;
+import com.ragequit.bottledup.physics.WorldManager;
 
 public class Entity 
 {
 	private BodyDef bodyDef;
 	private Body body;
+	private FixtureDef fixtureDef;
+	private Fixture fixture;
+	private Shape collisionShape;
 	
 	Entity()
 	{
-		setBodyDef(BodyType.StaticBody);
+		createBody(BodyType.StaticBody, new PolygonShape(), new FixtureDef());
 	}
 	
-	protected void setBodyDef(BodyType type)
+	protected void createBody(BodyType type, Shape shape, FixtureDef def)
 	{
 		bodyDef = new BodyDef();
 		bodyDef.type = BodyType.DynamicBody;
-		body = new world.createBody(bodyDef);
+		body = WorldManager.world.createBody(bodyDef);		
+		collisionShape = shape;
+		fixtureDef = def;
+		fixture = body.createFixture(fixtureDef);
+	}
+	
+	public void dispose()
+	{
+		collisionShape.dispose();
 	}
 }
--- core/src/com/ragequit/bottledup/entities/Player.java
+++ core/src/com/ragequit/bottledup/entities/Player.java
@@ -2,11 +2,16 @@ package com.ragequit.bottledup.entities;
 
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 
 public class Player extends Actor
 {
 	Player()
 	{
-		setBodyDef(BodyType.KinematicBody);
+		PolygonShape box = new PolygonShape();
+		box.setAsBox(1,1);
+		FixtureDef def = new FixtureDef();
+		super.createBody(BodyType.KinematicBody, box, def);
 	}
 }
