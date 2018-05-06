85749af731fa92badadfcab70270614317da3800
NevinSkye <39011383+NevinSkye@users.noreply.github.com>
2018-05-05 20:32:19 -0600

    Add files via upload

--- core/assets/Tilesets/Tiles/Items.tsx
+++ core/assets/Tilesets/Tiles/Items.tsx
@@ -1,2 +1,181 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Items" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset name="Items" tilewidth="129" tileheight="71" tilecount="59" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/bomb.png"/>
+ </tile>
+ <tile id="1">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/bombFlash.png"/>
+ </tile>
+ <tile id="2">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/bush.png"/>
+ </tile>
+ <tile id="3">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonBlue.png"/>
+ </tile>
+ <tile id="4">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonBlue_pressed.png"/>
+ </tile>
+ <tile id="5">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonGreen.png"/>
+ </tile>
+ <tile id="6">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonGreen_pressed.png"/>
+ </tile>
+ <tile id="7">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonRed.png"/>
+ </tile>
+ <tile id="8">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonRed_pressed.png"/>
+ </tile>
+ <tile id="9">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonYellow.png"/>
+ </tile>
+ <tile id="10">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/buttonYellow_pressed.png"/>
+ </tile>
+ <tile id="11">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/cactus.png"/>
+ </tile>
+ <tile id="12">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/chain.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="71" source="platformerGraphicsDeluxe_Updated/Items/cloud1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="129" height="71" source="platformerGraphicsDeluxe_Updated/Items/cloud2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="129" height="71" source="platformerGraphicsDeluxe_Updated/Items/cloud3.png"/>
+ </tile>
+ <tile id="16">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/coinBronze.png"/>
+ </tile>
+ <tile id="17">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/coinGold.png"/>
+ </tile>
+ <tile id="18">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/coinSilver.png"/>
+ </tile>
+ <tile id="19">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/fireball.png"/>
+ </tile>
+ <tile id="20">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagBlue.png"/>
+ </tile>
+ <tile id="21">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagBlue2.png"/>
+ </tile>
+ <tile id="22">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagBlueHanging.png"/>
+ </tile>
+ <tile id="23">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagGreen.png"/>
+ </tile>
+ <tile id="24">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagGreen2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagGreenHanging.png"/>
+ </tile>
+ <tile id="26">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagRed.png"/>
+ </tile>
+ <tile id="27">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagRed2.png"/>
+ </tile>
+ <tile id="28">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagRedHanging.png"/>
+ </tile>
+ <tile id="29">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagYellow.png"/>
+ </tile>
+ <tile id="30">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagYellow2.png"/>
+ </tile>
+ <tile id="31">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/flagYellowHanging.png"/>
+ </tile>
+ <tile id="32">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/gemBlue.png"/>
+ </tile>
+ <tile id="33">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/gemGreen.png"/>
+ </tile>
+ <tile id="34">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/gemRed.png"/>
+ </tile>
+ <tile id="35">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/gemYellow.png"/>
+ </tile>
+ <tile id="36">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/keyBlue.png"/>
+ </tile>
+ <tile id="37">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/keyGreen.png"/>
+ </tile>
+ <tile id="38">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/keyRed.png"/>
+ </tile>
+ <tile id="39">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/keyYellow.png"/>
+ </tile>
+ <tile id="40">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/mushroomBrown.png"/>
+ </tile>
+ <tile id="41">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/mushroomRed.png"/>
+ </tile>
+ <tile id="42">
+  <image width="19" height="14" source="platformerGraphicsDeluxe_Updated/Items/particleBrick1a.png"/>
+ </tile>
+ <tile id="43">
+  <image width="21" height="21" source="platformerGraphicsDeluxe_Updated/Items/particleBrick1b.png"/>
+ </tile>
+ <tile id="44">
+  <image width="19" height="14" source="platformerGraphicsDeluxe_Updated/Items/particleBrick2a.png"/>
+ </tile>
+ <tile id="45">
+  <image width="21" height="21" source="platformerGraphicsDeluxe_Updated/Items/particleBrick2b.png"/>
+ </tile>
+ <tile id="46">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/plant.png"/>
+ </tile>
+ <tile id="47">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/plantPurple.png"/>
+ </tile>
+ <tile id="48">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/rock.png"/>
+ </tile>
+ <tile id="49">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/snowhill.png"/>
+ </tile>
+ <tile id="50">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/spikes.png"/>
+ </tile>
+ <tile id="51">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/springboardDown.png"/>
+ </tile>
+ <tile id="52">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/springboardUp.png"/>
+ </tile>
+ <tile id="53">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/star.png"/>
+ </tile>
+ <tile id="54">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/switchLeft.png"/>
+ </tile>
+ <tile id="55">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/switchMid.png"/>
+ </tile>
+ <tile id="56">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/switchRight.png"/>
+ </tile>
+ <tile id="57">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/weight.png"/>
+ </tile>
+ <tile id="58">
+  <image width="70" height="70" source="platformerGraphicsDeluxe_Updated/Items/weightChained.png"/>
+ </tile>
+</tileset>
--- core/assets/Tilesets/Tiles/Sky.tsx
+++ core/assets/Tilesets/Tiles/Sky.tsx
@@ -1,2 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Sky" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset name="Sky" tilewidth="2304" tileheight="1440" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="2304" height="1440" source="Background.jpg"/>
+ </tile>
+</tileset>
