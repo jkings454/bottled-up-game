8fe6b3e2c17dfdd9246fd472363b0530e60c4f18
NevinSkye <39011383+NevinSkye@users.noreply.github.com>
2018-05-05 20:29:03 -0600

    Add files via upload

--- core/assets/Tilesets/Tiles/Items.tsx
+++ core/assets/Tilesets/Tiles/Items.tsx
@@ -1,4 +1,2 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Items" tilewidth="72" tileheight="72" tilecount="64" columns="8">
- <image source="../Items/items_spritesheet.png" width="576" height="576"/>
-</tileset>
+<tileset name="Items" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
